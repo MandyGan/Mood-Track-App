80c4865f68be96d2c21ae1736ac90f7efd8efbdb
Lingling Gan <linglinggan123@gmail.com>
2023-07-05 14:14:08 -0700

    Add the css class to ViewReport Component

--- src/components/ViewReport.js
+++ src/components/ViewReport.js
@@ -3,7 +3,7 @@ import { APP_STATES } from "../utils/constants";
 
 export const ViewReport = ({ setCurrentScreen, ratings }) => {
   return (
-    <div>
+    <div className="viewReport">
       {ratings.join(", ")}
       <button onClick={() => setCurrentScreen(APP_STATES.home)}>Back</button>
     </div>

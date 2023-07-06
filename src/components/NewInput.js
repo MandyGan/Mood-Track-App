96a4872d8bcf6b034acdcd1af416485ec0c543a4
Lingling Gan <linglinggan123@gmail.com>
2023-07-05 20:56:23 -0700

    Add handleSubmit function to the form

--- src/components/NewInput.js
+++ src/components/NewInput.js
@@ -1,11 +1,17 @@
 import { useState } from "react";
 import { APP_STATES } from "../utils/constants";
+import "./NewInput.css";
 
-export const NewInput = (setFeelings, setCurrentScreen) => {
+export const NewInput = ({ setFeelings, setCurrentScreen }) => {
   const [input, setInput] = useState("");
   const [isValid, setIsValid] = useState(false);
-  const handleSubmit = (feeling) => {
-    setCurrentScreen(APP_STATES.input);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (isValid) {
+      setFeelings((prevFeelings) => [...prevFeelings, input.trim()]);
+      setInput("");
+      setCurrentScreen(APP_STATES.home); // Navigate back to the home screen after submitting
+    }
   };
   const validate = (feeling) => {
     if (feeling.length === 0) {
@@ -19,20 +25,24 @@ export const NewInput = (setFeelings, setCurrentScreen) => {
     setInput(e.target.value);
   };
   return (
-    <form onSubmit={handleSubmit}>
-      <h1>You feeling at the moment is: {input}</h1>
-      <label htmlFor="feeling"></label>
-      <input
-        type="text"
-        placeholder="type how you feel!"
-        name="feeling"
-        id="feeling"
-        onChange={handleChange}
-        value={input}
-      />
-      {!isValid && <p style={{ color: "red" }}>The feeling can not be empty</p>}
-      <button>Submit</button>
-    </form>
+    <div className="newInput">
+      <form onSubmit={handleSubmit}>
+        <h1>You feeling at the moment is: {input}</h1>
+        <label htmlFor="feeling"></label>
+        <input
+          type="text"
+          placeholder="type how you feel!"
+          name="feeling"
+          id="feeling"
+          onChange={handleChange}
+          value={input}
+        />
+        {!isValid && (
+          <p style={{ color: "red" }}>The feeling can not be empty</p>
+        )}
+        <button>Submit</button>
+      </form>
+    </div>
   );
 };
 

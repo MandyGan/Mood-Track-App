ec461ad22271f3eeb5d3355b7e702315d116d216
Lingling Gan <linglinggan123@gmail.com>
2023-07-06 15:18:30 -0700

    Render LineChart on ViewReport

--- src/components/ViewReport.js
+++ src/components/ViewReport.js
@@ -1,11 +1,25 @@
-import "./ViewReport.css";
+import React from "react";
 import { APP_STATES } from "../utils/constants";
+import { LineChart } from "./LineChart";
 
-export const ViewReport = ({ setCurrentScreen, ratings, feelings }) => {
+export const ViewReport = ({ setCurrentScreen, ratings, feelingsData }) => {
   return (
     <div className="viewReport">
-      <div>{ratings.join(", ")}</div>
-      <div>{feelings.join(", ")}</div>
+      <div>
+        <h1>Rating Chart</h1>
+        <LineChart ratings={ratings} />
+      </div>
+      <div>
+        <h2>Feelings:</h2>
+        <ul>
+          {feelingsData.map((feeling, index) => (
+            <li key={index}>
+              Feeling: {feeling.text}, Count: {feeling.count}, Timestamps:{" "}
+              {feeling.timestamps.join(", ")}
+            </li>
+          ))}
+        </ul>
+      </div>
       <button onClick={() => setCurrentScreen(APP_STATES.home)}>Back</button>
     </div>
   );

0479a53388fa452302461f792fb82db82e36145d
Lingling Gan <linglinggan123@gmail.com>
2023-07-08 13:14:15 -0700

    Add MUI Tab to ViewReport

--- src/components/ViewReport.js
+++ src/components/ViewReport.js
@@ -1,18 +1,32 @@
 import React from "react";
-import { APP_STATES } from "../utils/constants";
+import Tabs from "@mui/joy/Tabs";
+import TabList from "@mui/joy/TabList";
+import Tab from "@mui/joy/Tab";
+import TabPanel from "@mui/joy/TabPanel";
+import { LineChart } from "./LineChart";
+import { FeelingWordCloud } from "./FeelingWordCloud";
 
-export const ViewReport = ({ setCurrentScreen }) => {
+export const ViewReport = ({ ratings, feelingsData, setCurrentScreen }) => {
   return (
-    <div className="viewReport">
-      <button onClick={() => setCurrentScreen(APP_STATES.lineChart)}>
-        Your Mood Chart
-      </button>
-      <div>
-        <button onClick={() => setCurrentScreen(APP_STATES.wordCloud)}>
-          Your Feeling Cloud
-        </button>
-      </div>
-      <button onClick={() => setCurrentScreen(APP_STATES.home)}>Back</button>
+    <div>
+      <Tabs
+        aria-label="Basic tabs"
+        defaultValue={0}
+        sx={{ borderRadius: "lg" }}>
+        <TabList>
+          <Tab>Mood Chart</Tab>
+          <Tab>Feelings Word Cloud</Tab>
+        </TabList>
+        <TabPanel value={0} sx={{ p: 2 }}>
+          <LineChart ratings={ratings} setCurrentScreen={setCurrentScreen} />
+        </TabPanel>
+        <TabPanel value={1} sx={{ p: 2 }}>
+          <FeelingWordCloud
+            feelingsData={feelingsData}
+            setCurrentScreen={setCurrentScreen}
+          />
+        </TabPanel>
+      </Tabs>
     </div>
   );
 };

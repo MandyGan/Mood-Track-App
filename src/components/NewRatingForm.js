0c217fff66d2e212c7d7e06ab35d4f77ec79b013
Lingling Gan <linglinggan123@gmail.com>
2023-07-08 13:13:58 -0700

    Add h1 to NewRatingForm

--- src/components/NewRatingForm.js
+++ src/components/NewRatingForm.js
@@ -1,21 +1,7 @@
 import { APP_STATES } from "../utils/constants";
 import "./NewRatingForm.css";
 import { EmojiButton } from "./EmojiButton";
-
-// function getRandomInt(max) {
-//   return Math.floor(Math.random() * max);
-// }
-
-// function generateFakeRatings() {
-//   const ratings = [];
-
-//   for (let i = 0; i < 40; i++) {
-//     ratings.push({
-//       rating: getRandomInt(3) * (Math.random() > 0.5 ? 1 : -1),
-//       timestamp: new Date().getTime() - i * 1000 * 60 * 60,
-//     });
-//   }
-// }
+import Button from "@mui/joy/Button";
 
 export const NewRatingForm = ({ setRatings, setCurrentScreen }) => {
   const handleButtonClick = (rating) => {
@@ -27,31 +13,42 @@ export const NewRatingForm = ({ setRatings, setCurrentScreen }) => {
 
   return (
     <div className="newRatingForm">
-      <EmojiButton
-        handleClick={() => handleButtonClick(-1)}
-        text={-2}
-        color={"red"}
-      />
-      <EmojiButton
-        handleClick={() => handleButtonClick(-1)}
-        text={-1}
-        color={"orange"}
-      />
-      <EmojiButton
-        handleClick={() => handleButtonClick(0)}
-        text={0}
-        color={"yellow"}
-      />
-      <EmojiButton
-        handleClick={() => handleButtonClick(1)}
-        text={1}
-        color={"green"}
-      />
-      <EmojiButton
-        handleClick={() => handleButtonClick(2)}
-        text={2}
-        color={"blue"}
-      />
+      <h1 className="h1">How are you feeling?</h1>
+      <div className="buttonContainer">
+        <EmojiButton
+          handleClick={() => handleButtonClick(-1)}
+          text={-2}
+          color={"red"}
+        />
+        <EmojiButton
+          handleClick={() => handleButtonClick(-1)}
+          text={-1}
+          color={"orange"}
+        />
+        <EmojiButton
+          handleClick={() => handleButtonClick(0)}
+          text={0}
+          color={"yellow"}
+        />
+        <EmojiButton
+          handleClick={() => handleButtonClick(1)}
+          text={1}
+          color={"green"}
+        />
+        <EmojiButton
+          handleClick={() => handleButtonClick(2)}
+          text={2}
+          color={"blue"}
+        />
+      </div>
+      <div className="button">
+        <Button
+          size="md"
+          color="primary"
+          onClick={() => setCurrentScreen(APP_STATES.home)}>
+          Back to Home
+        </Button>
+      </div>
     </div>
   );
 };

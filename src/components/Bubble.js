268bf744a29caf17068c4c5a761d8d17cb1c94b3
Lingling Gan <linglinggan123@gmail.com>
2023-07-14 13:44:06 -0700

    Update the text in the bubble component

--- src/components/Bubble.js
+++ src/components/Bubble.js
@@ -4,10 +4,14 @@ import IconButton from "@mui/material/IconButton";
 import "./Bubble.css";
 
 export const Bubble = ({ clicked, onClose }) => {
+  let time = "time";
+  if (clicked.count > 1) {
+    time = "times";
+  }
   return (
     <div className="bubble">
       <p>
-        {`You felt ${clicked.text} for ${clicked.count} times!`}{" "}
+        {`You felt ${clicked.text} for ${clicked.count} ${time}!`}{" "}
         <IconButton onClick={onClose} aria-label="delete">
           <DeleteIcon />
         </IconButton>

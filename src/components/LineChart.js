22f524834aa8824f4ded2091c5ae49b579a1b3cc
Lingling Gan <linglinggan123@gmail.com>
2023-07-14 13:45:07 -0700

    Add time range in lineChart component

--- src/components/LineChart.js
+++ src/components/LineChart.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { useState } from "react";
 import Select from "@mui/joy/Select";
 import Option from "@mui/joy/Option";
 import Button from "@mui/joy/Button";
 import { APP_STATES } from "../utils/constants";
+import { getTimeRange } from "../utils/mathFunctions";
 import "./LineChart.css";
 import {
   Chart as ChartJS,
@@ -28,16 +29,26 @@ ChartJS.register(
 );
 
 export const LineChart = ({ ratings, setCurrentScreen }) => {
-  const [units, setUnits] = useState("minute");
+  const [units, setUnits] = useState("month");
+
   const handleOnUnitsChange = (evt, newValue) => {
     setUnits(newValue);
   };
+
+  const filteredRatings = useMemo(() => {
+    const currentTime = new Date().getTime();
+    const timeRange = getTimeRange(units);
+    return ratings.filter(
+      (rating) => currentTime - rating.timestamp < timeRange
+    );
+  }, [ratings, units]);
+
   const chartData = {
-    labels: ratings.map((rating) => rating.timestamp),
+    labels: filteredRatings.map((rating) => rating.timestamp),
     datasets: [
       {
         label: "Rating",
-        data: ratings.map((rating) => rating.rating),
+        data: filteredRatings.map((rating) => rating.rating),
         fill: true,
         borderColor: "green",
         backgroundColor: "rgba(255, 99, 132, 0.5)",
@@ -47,12 +58,18 @@ export const LineChart = ({ ratings, setCurrentScreen }) => {
   };
 
   // Configure the chart options
+  const xAxisUnitsMap = {
+    hour: "minute",
+    day: "hour",
+    week: "day",
+    month: "day",
+  };
   const chartOptions = {
     scales: {
       x: {
         type: "time",
         time: {
-          unit: units,
+          unit: xAxisUnitsMap[units],
         },
       },
       y: {
@@ -74,11 +91,10 @@ export const LineChart = ({ ratings, setCurrentScreen }) => {
         value={units}
         defaultValue="minute"
         onChange={handleOnUnitsChange}>
-        <Option value="minute">Minute</Option>
-        <Option value="hour">Hour</Option>
-        <Option value="day">Day</Option>
-        <Option value="week">Week</Option>
-        <Option value="month">Month</Option>
+        <Option value="hour">Last Hour</Option>
+        <Option value="day">Last Day</Option>
+        <Option value="week">Past 7 Days</Option>
+        <Option value="month">Past 30 Days</Option>
       </Select>
       <div className="chartWrapper">
         <Line data={chartData} options={chartOptions} />
